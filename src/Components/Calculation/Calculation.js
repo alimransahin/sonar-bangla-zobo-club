554af5ac5fca30af2b234ff50380414222c83cbd
alimransahin <92633905+alimransahin@users.noreply.github.com>
2022-09-29 08:10:12 +0600

    design Complete

--- src/Components/Calculation/Calculation.js
+++ src/Components/Calculation/Calculation.js
@@ -7,13 +7,13 @@ const Calculation = () => {
                 <h2 className=''>Activity Details</h2>
                 <div className="d-flex justify-content-between my-2 p-3 bg-secondary rounded-4 text-light">
                     <h4>Activity Time</h4>
-                    <h5 className=''>Activity Time</h5>
+                    <h5>0 Minutes</h5>
                 </div>
                 <div className="d-flex justify-content-between my-2 p-3 bg-secondary rounded-4 text-light">
-                    <h4>Activity Time</h4>
-                    <h5 className='text-muted'>Activity Time</h5>
+                    <h4>Break</h4>
+                    <h5>0 Minutes</h5>
                 </div>
-                
+                <button className='btn btn-primary my-2 p-3 rounded-4 w-100 fs-3' >Activity Completed</button>
             </div>
         </div>
     );
